ff6e091819e35a0f59b9e6b43777891ac020a1ae
Nils <nilsstnurfuersam@gmail.com>
2018-01-12 19:10:59 +0100

    add more sexy

--- src/ki/robotics/client/Main.java
+++ src/ki/robotics/client/Main.java
@@ -10,38 +10,37 @@ import java.net.InetAddress;
  * @version 1.0, 12/26/17
  */
 public class Main {
-    public static String HOST = "localhost";
+    public static final String HOST;
     public static final int PORT = 9999;
 
+    static {
+        int oneSecondTimeout = 1000;
+        String hostToTry = "192.168.43.171";
+        String hostToSet;
+
+        try {
+            if (InetAddress.getByName(hostToTry).isReachable(oneSecondTimeout)) {
+                hostToSet = hostToTry;
+            } else {
+                hostToSet = "localhost";
+            }
+        } catch (IOException e) {
+            hostToSet = "localhost";
+            e.printStackTrace();
+        }
+        HOST = hostToSet;
+    }
     /**
      * Program-Initialization and start of a Communicator.
      *
      * @param args
      */
     public static void main(String[] args) {
-        setHOST();
-        if (args.length >=1 && args[0].equals("terminal")) {
+        if (args.length >= 1 && args[0].equals("terminal")) {
             ComController comController = new TerminalComController();
             comController.start(null);
         } else {
             ComController comController = new GUIComController();
         }
     }
-
-    /**
-     * sets HOST to EV3 adress if EV3 is reachable
-     * else uses already set HOST
-     */
-    private static void setHOST() {
-        int oneSecondTimeout = 1000;
-        String hostToTry = "192.168.43.171";
-        try {
-            if (InetAddress.getByName(hostToTry).isReachable(oneSecondTimeout)) {
-                HOST = hostToTry;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
 }
