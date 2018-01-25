55481744bb6d00f61c6fd8e60a134e073d84e873
Markus Großhäuser <markus@grosshaeuser.de>
2018-01-25 09:39:33 +0100

    Merged CameraIntegration with master.

--- src/ki/robotics/utility/pixyCam/PixyCam.java
+++ src/ki/robotics/utility/pixyCam/PixyCam.java
@@ -9,7 +9,7 @@ public class PixyCam extends I2CSensor{
     private static final int SECOND_BYTE = 0x0000FF00;
 
     private static final int GENERAL_QUERY_ADDRESS = 0x50;
-    private static final int SIGNATURE_QUERY_BASE_ADDRESS = 0x50;;
+    private static final int SIGNATURE_QUERY_BASE_ADDRESS = 0x50;
     private static final int ANGLE_QUERY_ADDRESS = 0x60;
     private static final int COLOR_CODE_QUERY_ADDRESS = 0x58;
 
@@ -29,7 +29,6 @@ public class PixyCam extends I2CSensor{
 
 
     public DTOGeneralQuery generalQuery() {
-        sendData(GENERAL_QUERY_ADDRESS, new byte[]{0}, 0);
         byte[] buffer = new byte[BYTES_IN_GENERAL_QUERY_RESPONSE];
         getData(GENERAL_QUERY_ADDRESS, buffer, BYTES_IN_GENERAL_QUERY_RESPONSE);
         return  new DTOGeneralQuery(buffer);
@@ -42,7 +41,6 @@ public class PixyCam extends I2CSensor{
         }
 
         int address = SIGNATURE_QUERY_BASE_ADDRESS + signature;
-        sendData(address, new byte[]{0}, 0);
         byte[] buffer = new byte[BYTES_IN_SIGNATURE_QUERY_RESPONSE];
         getData(address, buffer, BYTES_IN_SIGNATURE_QUERY_RESPONSE);
         return new DTOSignatureQuery(buffer);
@@ -58,7 +56,6 @@ public class PixyCam extends I2CSensor{
 
 
     public DTOAngleQuery angleQuery() {
-        sendData(ANGLE_QUERY_ADDRESS, new byte[]{0}, 0);
         byte[] buffer = new byte[BYTES_IN_ANGLE_QUERY_RESPONSE];
         getData(ANGLE_QUERY_ADDRESS, buffer, BYTES_IN_ANGLE_QUERY_RESPONSE);
         return new DTOAngleQuery(buffer);
