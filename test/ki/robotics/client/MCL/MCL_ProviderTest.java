87169d8d9be54eddddbd3d8bc62d61b1c2c9260f
nils <nils>
2018-04-04 21:15:00 +0200

    first turnFull test

--- test/ki/robotics/client/MCL/MCL_ProviderTest.java
+++ test/ki/robotics/client/MCL/MCL_ProviderTest.java
@@ -1,12 +1,19 @@
 package ki.robotics.client.MCL;
 
+import ki.robotics.robot.MCLParticle;
+import ki.robotics.utility.map.Map;
+import ki.robotics.utility.map.MapProvider;
+import lejos.robotics.navigation.Pose;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
 
 public class MCL_ProviderTest {
+
 
     @BeforeMethod
     public void setUp() {
@@ -53,6 +60,52 @@ public class MCL_ProviderTest {
     }
 
     @Test
-    public void testTurnFull() {
+    public void testTurnFull(MCL_Provider mclP) {
+        int[] angles = new int[]{0, 90, 110, 360};
+        for (int turnAngle : angles) {
+            //test values
+            int maxAngle = 360;
+            int maxGauss = 10;
+            int gaussRange = Math.round(turnAngle * (1 + (maxGauss / 540)));
+
+            //Testing
+            for (int i = 0; i < mclP.getParticles().size(); i++) {
+
+                MCLParticle particle = mclP.getParticles().get(i);
+                Pose pose = particle.getPose();
+
+                float postHeadingShould = (pose.getHeading() + turnAngle);
+                mclP.turnFull(turnAngle);
+                float postHeadingIs = pose.getHeading();
+
+                Assert.assertTrue(gaussRange >= Math.abs(postHeadingShould - postHeadingIs) || maxAngle - gaussRange <= Math.abs(postHeadingShould - postHeadingIs));
+            }
+        }
+    }
+
+    @DataProvider(name = "getConfigurationProvider")
+    public Object[][] getConfigurationProvider() {
+        int step = 10; //via GUI in cm
+        int numOfParticles = 1000; //via GUI
+        int tolerance = 10; //via GUI in cm
+        String mapKey = Configuration.ConfigTwoD.DEFAULT.getMapKey();
+        Configuration config2D = new Configuration.ConfigTwoD(
+                mapKey,
+                false,
+                true,
+                false,
+                step,
+                numOfParticles,
+                true,
+                tolerance,
+                true,
+                true,
+                true,
+                true,
+                true);
+        Map map = MapProvider.getInstance().getMap("Room");
+        int[] limitations = MapProvider.getInstance().getMapLimitations(config2D.getMapKey());
+
+        return new Object[][] {{new MCL_Provider(map, config2D.getNumberOfParticles(), limitations, config2D) }, {new MCL_Provider(map, config2D.getNumberOfParticles(), limitations, config2D)},};
     }
 }
